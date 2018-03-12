02dc09558b9bb3de559a3294a7b30409a9bcfa93
Assyfa Amany <assyfaamany@gmail.com>
2018-03-12 15:06:39 +0800

    Added initial folder

--- App.js
+++ App.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
-import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+import { AppRegistry, StyleSheet, Text, View, Image, Button, Alert } from 'react-native';
+
+
 
 export default class LotsOfStyles extends Component {
+  handleEvent() {
+    Alert.alert('You clicked a button!');
+   }
   render() {
+
     return (
       <View>
-        
+        <Text style={{marginTop:50}}></Text>
         <Text style={styles.bigpurple}>Welcome</Text>
-        <Text style={styles.bigpurple}></Text>
-        
+        <Text style={styles.red}>Hi, it's Syfa</Text>
+        <Image
+          style={{width: 500, height: 400, alignSelf:'center', marginTop:50}}
+          source={{uri: 'https://upload.wikimedia.org/wikipedia/commons/b/b2/Endangered_Red_Panda.jpg'}}
+        />
+        <View style={styles.buttonContainer}>
+          <Button onPress={this.handleEvent} title="Click Me" />
+        </View>
       </View>
     );
   }
@@ -25,9 +37,22 @@ const styles = StyleSheet.create({
     color: 'red',
     textAlign: 'center',
   },
+  buttonContainer: {
+    backgroundColor: '#2E9298',
+    borderRadius: 10,
+    padding: 10,
+    width: 300,
+    marginTop:40,
+    alignSelf: 'center',
+    shadowColor: '#000000',
+    shadowOffset: {
+      width: 0,
+      height: 3
+    },
+    shadowRadius: 10,
+    shadowOpacity: 0.25
+  }
 });
 
 
-
-// skip this line if using Create React Native App
 AppRegistry.registerComponent('AwesomeProject', () => LotsOfStyles);
